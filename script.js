e6d318a74a2936e0847488b454d0cbf59a7382b9
Giuseppe Rizza <ilmoro86@hotmail.it>
2025-01-21 23:23:36 +0100

    [feat] Add all cards

--- script.js
+++ script.js
@@ -36,3 +36,53 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+
+// prendo l'elemento dalla pagina
+const outputCard = document.querySelector(".containerCard");
+
+// inizializzo la variabile 
+items = "";
+
+// creo la card dall'array dato
+
+for (i = 0; i < teamMembers.length; i++) {
+
+  // salvo il valore corrente
+  const membroIesimo = teamMembers[i];  
+
+  // invoco la funzione e l'aggiungo alla stringa contenuta in items
+  items += creaCardMembro(membroIesimo)
+
+}
+
+// inserisco output della card in pagina
+outputCard.innerHTML = items;
+
+
+// FUNZIONI
+
+//  funzione che crea la card di un membro
+function creaCardMembro(oggetto) {
+
+  // prendo i valori delle proprietà l'oggetto dato
+  const { name, role, email, img } = oggetto;
+
+  // inizializzo la variabile 
+  let item = "";
+
+  // creo l'html da inserire in pagina
+  item += `
+  <div class="card">
+    <div class="cardImage">
+       <img src="./${img}" alt="${name}">
+    </div>
+    <div class="cardText">
+       <h3>${name}</h3>
+       <div id="roleText">${role}</div>
+    </div>
+  </div>
+`
+  return item;
+
+}
